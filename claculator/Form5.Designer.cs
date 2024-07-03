0c53b6ae54bf81f42388a2c2392ab977b34f5c78
Amirsarkarpour <amir.asa.nasa@gmail.com>
2024-07-03 18:28:48 +0330

    Form5

---
+++ claculator/Form5.Designer.cs
@@ -0,0 +1,482 @@
+﻿namespace claculator
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            euro2 = new Button();
+            dollar2 = new Button();
+            secondlabel = new Label();
+            euro1 = new Button();
+            dollar1 = new Button();
+            firstlable = new Label();
+            secondn = new Label();
+            firstn = new Label();
+            button2 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button11 = new Button();
+            button13 = new Button();
+            button12 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button6 = new Button();
+            button3 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            reall2 = new Button();
+            reall1 = new Button();
+            SuspendLayout();
+            // 
+            // euro2
+            // 
+            euro2.BackColor = Color.FromArgb(50, 50, 50);
+            euro2.FlatAppearance.BorderSize = 0;
+            euro2.FlatStyle = FlatStyle.Flat;
+            euro2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            euro2.ForeColor = Color.WhiteSmoke;
+            euro2.Location = new Point(57, 234);
+            euro2.Name = "euro2";
+            euro2.Size = new Size(94, 35);
+            euro2.TabIndex = 56;
+            euro2.Text = "euro";
+            euro2.UseVisualStyleBackColor = false;
+            euro2.Click += clickvahed2;
+            // 
+            // dollar2
+            // 
+            dollar2.BackColor = Color.FromArgb(50, 50, 50);
+            dollar2.FlatAppearance.BorderSize = 0;
+            dollar2.FlatStyle = FlatStyle.Flat;
+            dollar2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dollar2.ForeColor = Color.WhiteSmoke;
+            dollar2.Location = new Point(57, 191);
+            dollar2.Name = "dollar2";
+            dollar2.Size = new Size(94, 33);
+            dollar2.TabIndex = 55;
+            dollar2.Text = "dollar";
+            dollar2.UseVisualStyleBackColor = false;
+            dollar2.Click += clickvahed2;
+            // 
+            // secondlabel
+            // 
+            secondlabel.BackColor = Color.FromArgb(50, 50, 50);
+            secondlabel.Location = new Point(47, 191);
+            secondlabel.Name = "secondlabel";
+            secondlabel.Size = new Size(113, 125);
+            secondlabel.TabIndex = 50;
+            // 
+            // euro1
+            // 
+            euro1.BackColor = Color.FromArgb(50, 50, 50);
+            euro1.FlatAppearance.BorderSize = 0;
+            euro1.FlatStyle = FlatStyle.Flat;
+            euro1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            euro1.ForeColor = Color.WhiteSmoke;
+            euro1.Location = new Point(57, 110);
+            euro1.Name = "euro1";
+            euro1.Size = new Size(94, 35);
+            euro1.TabIndex = 52;
+            euro1.Text = "euro";
+            euro1.UseVisualStyleBackColor = false;
+            euro1.Click += clickvahed;
+            // 
+            // dollar1
+            // 
+            dollar1.BackColor = Color.FromArgb(50, 50, 50);
+            dollar1.FlatAppearance.BorderSize = 0;
+            dollar1.FlatStyle = FlatStyle.Flat;
+            dollar1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dollar1.ForeColor = Color.WhiteSmoke;
+            dollar1.Location = new Point(57, 74);
+            dollar1.Name = "dollar1";
+            dollar1.Size = new Size(94, 33);
+            dollar1.TabIndex = 51;
+            dollar1.Text = "dollar";
+            dollar1.UseVisualStyleBackColor = false;
+            dollar1.Click += clickvahed;
+            // 
+            // firstlable
+            // 
+            firstlable.BackColor = Color.FromArgb(50, 50, 50);
+            firstlable.Location = new Point(47, 64);
+            firstlable.Name = "firstlable";
+            firstlable.Size = new Size(113, 115);
+            firstlable.TabIndex = 49;
+            // 
+            // secondn
+            // 
+            secondn.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            secondn.ForeColor = Color.WhiteSmoke;
+            secondn.Location = new Point(33, 245);
+            secondn.Name = "secondn";
+            secondn.Size = new Size(140, 42);
+            secondn.TabIndex = 48;
+            secondn.Text = "dollar";
+            secondn.Click += second;
+            // 
+            // firstn
+            // 
+            firstn.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            firstn.ForeColor = Color.WhiteSmoke;
+            firstn.Location = new Point(33, 149);
+            firstn.Name = "firstn";
+            firstn.Size = new Size(140, 42);
+            firstn.TabIndex = 47;
+            firstn.Text = "reall";
+            firstn.Click += first;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(59, 59, 59);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button2.ForeColor = Color.WhiteSmoke;
+            button2.Location = new Point(138, 571);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 55);
+            button2.TabIndex = 44;
+            button2.Text = "0";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(59, 59, 59);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button5.ForeColor = Color.WhiteSmoke;
+            button5.Location = new Point(6, 517);
+            button5.Name = "button5";
+            button5.Size = new Size(129, 52);
+            button5.TabIndex = 32;
+            button5.Text = "1";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(59, 59, 59);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button4.ForeColor = Color.WhiteSmoke;
+            button4.Location = new Point(138, 517);
+            button4.Name = "button4";
+            button4.Size = new Size(128, 52);
+            button4.TabIndex = 43;
+            button4.Text = "2";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(59, 59, 59);
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button8.ForeColor = Color.WhiteSmoke;
+            button8.Location = new Point(6, 464);
+            button8.Name = "button8";
+            button8.Size = new Size(129, 51);
+            button8.TabIndex = 42;
+            button8.Text = "4";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(59, 59, 59);
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button7.ForeColor = Color.WhiteSmoke;
+            button7.Location = new Point(138, 464);
+            button7.Name = "button7";
+            button7.Size = new Size(128, 51);
+            button7.TabIndex = 41;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.FromArgb(59, 59, 59);
+            button11.FlatAppearance.BorderSize = 0;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button11.ForeColor = Color.WhiteSmoke;
+            button11.Location = new Point(6, 406);
+            button11.Name = "button11";
+            button11.Size = new Size(129, 56);
+            button11.TabIndex = 40;
+            button11.Text = "7";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.FromArgb(50, 50, 50);
+            button13.FlatAppearance.BorderSize = 0;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button13.ForeColor = Color.WhiteSmoke;
+            button13.Location = new Point(269, 353);
+            button13.Name = "button13";
+            button13.Size = new Size(125, 50);
+            button13.TabIndex = 39;
+            button13.Text = "⌫";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += Backspace;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(50, 50, 50);
+            button12.FlatAppearance.BorderSize = 0;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button12.ForeColor = Color.WhiteSmoke;
+            button12.Location = new Point(138, 353);
+            button12.Name = "button12";
+            button12.Size = new Size(128, 50);
+            button12.TabIndex = 38;
+            button12.Text = "CE";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += Clear;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(59, 59, 59);
+            button10.FlatAppearance.BorderSize = 0;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button10.ForeColor = Color.WhiteSmoke;
+            button10.Location = new Point(138, 406);
+            button10.Name = "button10";
+            button10.Size = new Size(128, 56);
+            button10.TabIndex = 37;
+            button10.Text = "8";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(59, 59, 59);
+            button9.FlatAppearance.BorderSize = 0;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button9.ForeColor = Color.WhiteSmoke;
+            button9.Location = new Point(269, 406);
+            button9.Name = "button9";
+            button9.Size = new Size(125, 56);
+            button9.TabIndex = 36;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(59, 59, 59);
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button6.ForeColor = Color.WhiteSmoke;
+            button6.Location = new Point(269, 464);
+            button6.Name = "button6";
+            button6.Size = new Size(125, 51);
+            button6.TabIndex = 35;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(59, 59, 59);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button3.ForeColor = Color.WhiteSmoke;
+            button3.Location = new Point(269, 517);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 52);
+            button3.TabIndex = 34;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(59, 59, 59);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            button1.ForeColor = Color.WhiteSmoke;
+            button1.Location = new Point(269, 571);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 55);
+            button1.TabIndex = 33;
+            button1.Text = "/";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.WhiteSmoke;
+            label1.Location = new Point(8, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(388, 55);
+            label1.TabIndex = 46;
+            label1.Text = "0";
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.WhiteSmoke;
+            label2.Location = new Point(11, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(383, 54);
+            label2.TabIndex = 45;
+            label2.Text = "0";
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Silver;
+            label3.Font = new Font("Segoe UI", 199.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(30, 56);
+            label3.Name = "label3";
+            label3.Size = new Size(341, 478);
+            label3.TabIndex = 57;
+            label3.Text = "X";
+            // 
+            // reall2
+            // 
+            reall2.BackColor = Color.FromArgb(50, 50, 50);
+            reall2.FlatAppearance.BorderSize = 0;
+            reall2.FlatStyle = FlatStyle.Flat;
+            reall2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            reall2.ForeColor = Color.WhiteSmoke;
+            reall2.Location = new Point(57, 275);
+            reall2.Name = "reall2";
+            reall2.Size = new Size(94, 35);
+            reall2.TabIndex = 56;
+            reall2.Text = "reall";
+            reall2.UseVisualStyleBackColor = false;
+            reall2.Click += clickvahed2;
+            // 
+            // reall1
+            // 
+            reall1.BackColor = Color.FromArgb(50, 50, 50);
+            reall1.FlatAppearance.BorderSize = 0;
+            reall1.FlatStyle = FlatStyle.Flat;
+            reall1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            reall1.ForeColor = Color.WhiteSmoke;
+            reall1.Location = new Point(57, 144);
+            reall1.Name = "reall1";
+            reall1.Size = new Size(94, 35);
+            reall1.TabIndex = 52;
+            reall1.Text = "reall";
+            reall1.UseVisualStyleBackColor = false;
+            reall1.Click += clickvahed;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(32, 32, 32);
+            ClientSize = new Size(400, 630);
+            Controls.Add(label3);
+            Controls.Add(reall2);
+            Controls.Add(euro2);
+            Controls.Add(dollar2);
+            Controls.Add(secondlabel);
+            Controls.Add(reall1);
+            Controls.Add(euro1);
+            Controls.Add(dollar1);
+            Controls.Add(firstlable);
+            Controls.Add(secondn);
+            Controls.Add(firstn);
+            Controls.Add(button2);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button11);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Name = "Form5";
+            Text = "Form5";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button euro2;
+        private Button dollar2;
+        private Label secondlabel;
+        private Button euro1;
+        private Button dollar1;
+        private Label firstlable;
+        private Label secondn;
+        private Label firstn;
+        private Button button2;
+        private Button button5;
+        private Button button4;
+        private Button button8;
+        private Button button7;
+        private Button button11;
+        private Button button13;
+        private Button button12;
+        private Button button10;
+        private Button button9;
+        private Button button6;
+        private Button button3;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button reall2;
+        private Button reall1;
+    }
+}
---
+++ claculator/Form5.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+namespace claculator
+{
+    public partial class Form5 : Form
+    {
+
+        Button numberr;
+        int firstsum = 0;
+        int secondsum = 0;
+        double vahed;
+        string dolar;
+        string euro;
+
+
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        double tabdil(string label1, string secondn, string firstn)
+        {
+
+            double label11 = Convert.ToDouble(label1);
+            double dollar = Convert.ToDouble(dolar);
+            double eurro = Convert.ToDouble(euro);
+
+
+            if (firstn == "dollar" && secondn == "euro")
+            {
+                dollar *= label11;
+                vahed = dollar / eurro;
+
+            }
+
+            else if (firstn == "euro" && secondn == "dollar")
+            {
+                eurro *= label11;
+
+                vahed = eurro / dollar;
+
+            }
+
+            else if (firstn == "reall" && secondn == "dollar")
+            {
+
+                vahed = label11 / dollar;
+
+            }
+
+            else if (firstn == "reall" && secondn == "euro")
+            {
+
+                vahed = label11 / eurro;
+
+            }
+
+            else if (firstn == "euro" && secondn == "reall")
+            {
+
+                ;
+                vahed = label11 * eurro;
+
+            }
+
+            else if (firstn == "dollar" && secondn == "reall")
+            {
+
+                vahed = label11 * dollar;
+
+            }
+
+            
+
+
+            return vahed;
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+            numberr = (Button)sender;
+
+            if (label1.Text == "0")
+            {
+                label1.Text = "";
+            }
+
+            label1.Text += numberr.Text;
+
+            label2.Text = tabdil(label1.Text, secondn.Text, firstn.Text) + "";
+
+
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+            label2.Text = "0";
+
+
+        }
+
+        private void Backspace(object sender, EventArgs e)
+        {
+            string vasettext = label1.Text;
+            vasettext = vasettext.Remove(vasettext.Length - 1);
+            label1.Text = vasettext;
+
+            label2.Text = tabdil(label1.Text, secondn.Text, firstn.Text) + "";
+        }
+
+        private void first(object sender, EventArgs e)
+        {
+            if (firstsum % 2 == 0)
+            {
+
+                firstlable.Show();
+                dollar1.Show();
+                euro1.Show();
+                reall1.Show();
+
+            }
+
+            else
+            {
+
+                firstlable.Hide();
+                dollar1.Hide();
+                euro1.Hide();
+                reall1.Hide();
+
+            }
+
+            firstsum++;
+        }
+
+
+
+        private void second(object sender, EventArgs e)
+        {
+            if (secondsum % 2 == 0)
+            {
+
+                secondlabel.Show();
+                dollar2.Show();
+                euro2.Show();
+                reall2.Show();
+
+            }
+
+            else
+            {
+
+                secondlabel.Hide();
+                dollar2.Hide();
+                euro2.Hide();
+                reall2.Hide();
+
+            }
+
+            secondsum++;
+        }
+
+        private void clickvahed(object sender, EventArgs e)
+        {
+            Button number = (Button)sender;
+
+            firstsum = 0;
+            firstlable.Hide();
+            dollar1.Hide();
+            euro1.Hide();
+            reall1.Hide();
+
+            firstn.Text = number.Text;
+
+            label2.Text = tabdil(label1.Text, secondn.Text, firstn.Text) + "";
+
+        }
+
+        private void clickvahed2(object sender, EventArgs e)
+        {
+            Button number = (Button)sender;
+
+            secondsum = 0;
+            secondlabel.Hide();
+            dollar2.Hide();
+            euro2.Hide();
+            reall2.Hide();
+
+            secondn.Text = number.Text;
+
+            label2.Text = tabdil(label1.Text, secondn.Text, firstn.Text) + "";
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            firstlable.Hide();
+            secondlabel.Hide();
+            dollar1.Hide();
+            dollar2.Hide();
+            euro1.Hide();
+            euro2.Hide();
+            reall1.Hide();
+            reall2.Hide();
+
+
+            string apiUrl = "http://api.navasan.tech/latest/?api_key=free88AXqQgj6y8rqhBIOFcdETngy8Dm";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+                    decimal doolerValue = data.usd_sell.value;
+                    decimal euroValue = data.eur.value;
+
+
+
+                    dolar = $" {doolerValue} ";
+                    
+                    euro = $" {euroValue} ";
+                }
+
+            }
+
+            label3.Hide();
+
+        }
+    }
+}
